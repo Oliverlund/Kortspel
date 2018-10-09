4ff1921fc229411ffc47455e09e6b32154b8b719
Oliver Lund <luol16@nti.umea>
2018-10-09 10:21:51 +0200

    det går dåligt

--- kortspel.py
+++ kortspel.py
@@ -2,35 +2,39 @@ print(" ♣ Välkommen till finns i sjön ♣ \n")
 print("1.Starta\n"
 +"2.Avsluta\n")
 
-# ♠ ♥ ♦ ♣
+# ♠ ♥ ♦ ♣ Spader, hjärter, ruter och klöver
 
-hjärter = 0
-spader = 0
-klöver = 0
-ruter = 0
-kort = 0
+kort = ["Ess", "2", "3", "4", "5","6", "7", "8", "9","10", "Knekt", "Dam","Kung"]
+typ = ["♠ Spader ♠"," ♥ Hjärter ♥", "♦ Ruter ♦", "♣ Klöver ♣"]
+
+hand = []
 
 meny = 0
 meny2 = 0
+ 
 
 while meny != 2:
-    
+
     try:
         meny = int(input("Gör ett val: "))
     except:
-        print("\nDu måste ange en siffra!\n") 
+        print("\nDu måste ange en siffra!\n")
+
     if meny == 1:
-       print("\nLoading…\n"
+        print("\nLoading…\n"
         +"\n██ 39% \n"
         +"\n███ 49% \n"
         +"\n████ 76% \n"
         +"\n█████ 89% \n"
         +"\n██████ 100%\n"
-        +"\ncomplete\n")
-        
-
-        # tal = random.randint(1, 12)
-         
+        +"\nComplete\n")
+    
     elif meny == 2:
         print ("\nSes snart igen\n")
-    
+
+
+#kort = random.randint(0, 12)
+
+#while kort != 100:
+    #typ = int(input("Vilken typ av kort: "))
+    #kort = int(input("Skriv in kort: "))
